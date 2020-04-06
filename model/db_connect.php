4f032509cd98c013b3d697b51e9fb91568314643
Raushan Kumar <scarporation@gmail.com>
2020-04-06 21:47:05 +0530

    Add files via upload

--- model/db_connect.php
+++ model/db_connect.php
@@ -1,1 +1,16 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=cp", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
